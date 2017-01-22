e9741c3a0c4133aee9269b3036619753103cf81b
Lucas Isabel <lucky.o.isa@gmail.com>
2017-01-22 20:10:16 -0200

    hover-menu

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 10) {
             $(".navbar").addClass("menuEffect");
-            $("#logo-img").attr('src','img/LogoBranco.png');
+            $("#logo-img").attr('src','../img/LogoBranco.png');
         } else {
             $(".navbar").removeClass("menuEffect");
-            $("#logo-img").attr('src','img/Logo.png');
+            $("#logo-img").attr('src','../img/Logo.png');
 
         }
     });
