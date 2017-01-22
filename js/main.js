68695bea0a820b503fffb2a7d9072681a90db6a1
Lucas Isabel <lucky.o.isa@gmail.com>
2017-01-22 18:34:11 -0200

    image hover

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function() {
-  $(window).on("scroll", function() {
-    if($(window).scrollTop() > 10) {
-      $(".navbar").addClass("menuEffect");
-    } else {
-      $(".navbar").removeClass("menuEffect");
-    }
-  });
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 10) {
+            $(".navbar").addClass("menuEffect");
+            $("#logo-img").attr("src", "img/LogoBranco.jpg");
+        } else {
+            $(".navbar").removeClass("menuEffect");
+            $("#logo-img").attr("src", "img/Logo.png");
+        }
+    });
 });
