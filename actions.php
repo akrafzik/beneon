c7b02a2498e4bf26486612a099625f9f09102c09
Fabio Marcell <fabio.marcell@outlook.com>
2017-01-22 16:01:33 -0200

    Cadastro de lead inserido

--- actions.php
+++ actions.php
@@ -1,7 +1,25 @@
 <?php 
-	include('functions.php'); 
+	include('first/functions.php'); 
 	$exec = filter_input( INPUT_POST, "exec" );
 	if ( $exec == "" ) {
-		die(var_dump("Foo"));
+		$nomeLead = filter_input( INPUT_POST, "nomeLead" );
+		$emailLead = filter_input( INPUT_POST, "emailLead", FILTER_SANITIZE_EMAIL );
+		$arrNome = explode(" ", trim($nomeLead));
+		
+
+		if ($nomeLead == "") {
+		    die(json_encode(array("status" => false, "message" => "Seu nome é obrigatório")));
+		}
+
+		if(count($arrNome) <= 1){
+			die(json_encode(array("status" => false, "message" => "Precisamos do seu nome e sobrenome!")));	
+		}
+
+		if (!filter_var($emailLead, FILTER_VALIDATE_EMAIL) === true) {
+		    die(json_encode(array("status" => false, "message" => "E-mail Inválido")));
+		}
+		
+		die(json_encode(inserttblLeed( array("leedNome" => $nomeLead, "leedEmail" => $emailLead) )));
+
 	}
 ?>
