475627b329d0615c39a65feccb1641dc9eb556e4
Lucas Isabel <lucky.o.isa@gmail.com>
2017-01-22 20:06:52 -0200

    navbar logo scroll

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 10) {
             $(".navbar").addClass("menuEffect");
+            $("#logo-img").attr('src','img/LogoBranco.png');
         } else {
             $(".navbar").removeClass("menuEffect");
-            $("#logo-img").attr('src','img/LogoBranco.png');
+            $("#logo-img").attr('src','img/Logo.png');
+
         }
     });
     $('.glyphicon-chevron-down').addClass('animated bounce');
