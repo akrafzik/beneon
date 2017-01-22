c1b935e578eedee232671bddf51958eb2767612a
Lucas Isabel <lucky.o.isa@gmail.com>
2017-01-22 18:56:27 -0200

    Logo changed

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ $(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 10) {
             $(".navbar").addClass("menuEffect");
-            $("#logo-img").attr("src", "img/LogoBranco.jpg");
         } else {
             $(".navbar").removeClass("menuEffect");
-            $("#logo-img").attr("src", "img/Logo.png");
         }
     });
 });
