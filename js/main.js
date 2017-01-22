c32b755af149f1a5659394f84eef25411f7b6099
Lucas Isabel <lucky.o.isa@gmail.com>
2017-01-22 02:01:08 -0200

    changed style

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ $(function() {
   $(window).on("scroll", function() {
     if($(window).scrollTop() > 10) {
       $(".navbar").addClass("menuEffect");
-      $(".navbar-default .navbar-nav>li>a:hover").addClass("backgroundMenu");
     } else {
       $(".navbar").removeClass("menuEffect");
-      $(".navbar-default .navbar-nav>li>a:hover").removeClass("backgroundMenu");
     }
   });
 });
