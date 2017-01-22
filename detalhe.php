54d11309028907ef44521e1d44e457eda021512b
Fabio Marcell <fabio.marcell@outlook.com>
2017-01-22 19:53:00 -0200

    Alteração da tag base

--- detalhe.php
+++ detalhe.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <base href="/">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/uikit.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
